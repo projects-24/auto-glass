d52d202aa9bd3817352124bfd36526e3acdd4a52
iddrismystic <funtechs45@gmail.com>
2025-02-26 02:38:41 +0000

    i

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,20 +2,25 @@ import UiButton from '@/ui/button'
 import Link from 'next/link'
 import React from 'react'
 import { PiCalendar } from 'react-icons/pi'
-
+import Image from 'next/image'
+import Logo from '@/ui/Logo'
 export default function Nav() {
   return (
     <div> <div class="navtop white">
     <div >
-      Call / WhatsApp: <span className="text-bold">+233 00988889</span>
+      <span className="h4">+1 647-708-4226 || +1 (905) 226-4226</span>
     </div>
     <div class="hline"></div>
     <div >
-      <a href="mailto:admin@gmail.com"  class="text-white">username@gmail.com</a>
+      <a href="mailto:info@autoglassgurus.ca"  >info@autoglassgurus.ca</a>
     </div>
   </div>
-  <div class="nav_bar card">
-     <a href="#home"> Logo</a>
+  <div class="nav_bar ">
+     {/* Logo with Image */}
+     <a href="#home">
+          <Logo />
+        </a>
+
   <div class="hide-small">
     <div class="row-flex gap">
       <a href="#home" class="nav_link text-white  text-minified">Home</a>
