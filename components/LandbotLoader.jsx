4f4f96413fd6ac0b568907f5a4f614873c11f08b
iddrismystic <funtechs45@gmail.com>
2025-06-03 14:19:52 +0000

    i

--- components/LandbotLoader.jsx
+++ components/LandbotLoader.jsx
@@ -1,37 +1,46 @@
-'use client';
-import { useEffect } from 'react';
+// 'use client';
+// import { useEffect } from 'react';
 
-const LandbotLoader = () => {
-  useEffect(() => {
-    const initLandbot = () => {
-      if (!window.myLandbot) {
-        const s = document.createElement('script');
-        s.type = 'module';
-        s.async = true;
+// const LandbotLoader = () => {
+//   useEffect(() => {
+//     const initLandbot = () => {
+//       if (!window.myLandbot) {
+//         const s = document.createElement('script');
+//         s.type = 'module';
+//         s.async = true;
 
-        s.addEventListener('load', () => {
-          window.myLandbot = new window.Landbot.Livechat({
-            configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-2933225-G882YTHAPDLKQQKD/index.json',
-          });
-        });
+//         s.addEventListener('load', () => {
+//           window.myLandbot = new window.Landbot.Livechat({
+//             configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-2933225-G882YTHAPDLKQQKD/index.json',
+//           });
+//         });
 
-        s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
+//         s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
 
-        const x = document.getElementsByTagName('script')[0];
-        x.parentNode.insertBefore(s, x);
-      }
-    };
+//         const x = document.getElementsByTagName('script')[0];
+//         x.parentNode.insertBefore(s, x);
+//       }
+//     };
 
-    window.addEventListener('mouseover', initLandbot, { once: true });
-    window.addEventListener('touchstart', initLandbot, { once: true });
+//     window.addEventListener('mouseover', initLandbot, { once: true });
+//     window.addEventListener('touchstart', initLandbot, { once: true });
 
-    return () => {
-      window.removeEventListener('mouseover', initLandbot);
-      window.removeEventListener('touchstart', initLandbot);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener('mouseover', initLandbot);
+//       window.removeEventListener('touchstart', initLandbot);
+//     };
+//   }, []);
 
-  return null;
-};
+//   return null;
+// };
 
-export default LandbotLoader;
+// export default LandbotLoader;
+
+
+import React from 'react'
+
+export default function LandbotLoader() {
+  return (
+    <></>
+  )
+}
